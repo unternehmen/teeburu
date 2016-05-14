10e50af43128f0c37d4af7603fa08bda9a1ce9d9
unternehmen <unternehmen@users.noreply.github.com>
2016-05-13 22:35:31 -0400

    Rewrite of script --- prettier and more powerful

--- teeburu.scm
+++ teeburu.scm
@@ -1,85 +1,38 @@
 #!/bin/sh
-/usr/bin/env guile-2.0 --debug --no-auto-compile -s $0 "$@" || exit 1
+set -e
+/usr/bin/env guile-2.0 --debug -s $0 "$@"
 exit 0
 !#
 
-(use-modules (sxml simple)
-             (rnrs bytevectors)
-             (ice-9 q)
-             (ice-9 match)
-             (srfi srfi-9)
-             (web uri)
-             (web request)
-             (web server))
+(use-modules (ice-9 match)      ; Pattern matching
+             (rnrs bytevectors) ; UTF-to-string converter
+             (srfi srfi-9)      ; Records
+             (sxml simple)      ; SXML to XML conversion
+             (web request)      ; HTTP request accessors
+             (web uri)          ; URI building and accessors
+             (web response)     ; HTTP response creator
+             (web server))      ; HTTP serving
+
+; configuration
+
+(define limit-of-posts 10)
+(define max-chars-in-title-field 64)
+(define max-chars-in-author-field 64)
+(define max-chars-in-content-field 1024)
+(define port 8080)
+(define host "127.0.0.1")
+
+; code
 
 (define-record-type <post>
   (make-post title author content)
   post?
-  (title post-title)
-  (author post-author)
+  (title   post-title)
+  (author  post-author)
   (content post-content))
-
-(define posts
-  (list
-    (make-post "(Test post) Help Wanted"
-               "Luigi"
-               "Seeking fellow lava cleaner for big spill.")
-    (make-post "(Test post) RP-News #1"
-               "ElephantFuzzy"
-               "Don Pianta arrests himself - Shop closed in east corner")))
-
-(define (generate-post pst)
-  (match pst (($ <post> title author content)
-    `(div (@ (class "post"))
-       (div (@ (class "title")) ,title) " "
-       (div (@ (class "author")) "[" ,author "]")
-       (div (@ (class "content")) ,content)))))
-
-(define (generate-page posts)
-  (string-append
-    (with-output-to-string
-      (lambda ()
-        (begin
-          (display "<!DOCTYPE html>\n")
-          (sxml->xml
-            `(html (@ (lang "en"))
-               (head
-                 (title "Teeburu")
-                 (link (@ (rel "stylesheet")
-                          (type "text/css")
-                          (href "style.css"))))
-               (body
-                 (h1 "Teeburu")
-                 (form (@ (action "/post/") (method "POST"))
-                   "Title: " (input (@ (name "title") (type "text")))
-                   "Author: " (input (@ (name "author") (type "text")))
-                   "Content: " (input (@ (name "content") (type "text")))
-                   (input (@ (type "submit") (value "Submit"))))
-                 ,@(map generate-post posts)))))))))
-
-(define (generate-stylesheet)
-  (with-output-to-string
-    (lambda ()
-      (for-each (lambda (line) (begin (display line) (newline)))
-        (list
-          "body {"
-          "  background: white;"
-          "  color: black"
-          "}"
-          ""
-          ".title { display: inline; font-weight: bold }"
-          ".author { display: inline }"
-          ""
-          ".post {"
-          "  margin: 1em;"
-          "  background: lightgray"
-          "}")))))
-
-; parse-url-encoded uses pattern matching to parse URL-encoded
-; strings into their unencoded forms.  For example, the code
-; (parse-escapes "Hello%2Bthere") returns the string "Hello+there".
-
+  
 (define (parse-url-encoded str)
+  "Parse a URL-encoded string into its unencoded form."
   (letrec ((translate
              (lambda (orig new str)
                (list->string
@@ -94,7 +47,8 @@ exit 0
              (lambda (acc rest)
                (match rest
                  (() acc)
-                 ((#\% (? char-is-hex? sixteens) (? char-is-hex? ones) . _)
+                 ((#\% (? char-is-hex? sixteens)
+                       (? char-is-hex? ones) . _)
                   (iter
                     (cons
                       (integer->char
@@ -107,43 +61,154 @@ exit 0
                                                        #\space
                                                        str)))))))
 
-(define (request-handler request request-body)
-  (let* ((uri (request-uri request))
-         (path (uri-path uri)))
-    (cond
-      ((string-ci=? path "/style.css")
-        (values
-          '((content-type . (text/css)))
-          (generate-stylesheet)))
-      ((string=? path "/")
-        (values
-          '((content-type . (text/html)))
-          (generate-page
-            posts)))
-      ((string=? path "/post/")
-        (begin
-          (set! posts
-                (let ((alist
-                        (map 
-                          (lambda (association)
-                            (match (string-split association #\=)
-                              ((key value)
-                                 (cons (string->symbol
-                                         (parse-url-encoded key))
-                                       (parse-url-encoded value)))))
-                          (string-split (utf8->string request-body) #\&))))
-                  (cons (make-post
-                          (cdr (assoc 'title alist))
-                          (cdr (assoc 'author alist))
-                          (cdr (assoc 'content alist)))
-                        posts)))
-          (values
-            '((content-type . (text/plain)))
-            "Post successful.")))
-      (else
-        (values
-          '((content-type . (text/plain)))
-          "404")))))
+(define (build-post-sxml post)
+  "Generate an SXML snippet representing a single post."
+  (match post
+    (($ <post> title author content)
+     `(div (@ (class "post"))
+        (div (@ (class "title"))
+          ,title) " "
+        (div (@ (class "author"))
+          "[" ,author "]") " "
+        (div (@ (class "content"))
+          ,content)))))
 
-(begin
-  (run-server request-handler))
+(define (build-main-page posts)
+  "Generate an HTML page showing the given posts."
+  (with-output-to-string
+    (lambda ()
+      (begin
+        (display "<!DOCTYPE html>")
+        (newline)
+        (sxml->xml
+          `(html (@ (lang "en"))
+             (head
+               (style (@ (type "text/css"))
+                 ,(string-append
+                    "label { "
+                    "display: inline-block; "
+                    "width: 5em; "
+                    "text-align: right "
+                    "} "
+                    ".button { padding-left: 5em } "
+                    ".post { background: lightgrey } "
+                    ".title { display: inline; font-weight: bold } "
+                    ".author { display: inline } ")))
+             (body
+               (h1 "Teeburu")
+               (p
+                 ,(format #f "Current posts: ~a" (length posts)))
+               (form (@ (action "#") (method "POST"))
+                 (p
+                   ,(format
+                      #f
+                      "Field limits: 0 < x <= ~a, 0 < x <= ~a, 0 < x <= ~a"
+                      max-chars-in-title-field
+                      max-chars-in-author-field
+                      max-chars-in-content-field))
+                 (div
+                   (label (@ (for "title")) "Title:") " "
+                   (input (@ (type "text")
+                             (id "title")
+                             (name "title"))))
+                 (div
+                   (label (@ (for "author")) "Author:") " "
+                   (input (@ (type "text")
+                             (id "author")
+                             (name "author"))))
+                 (div
+                   (label (@ (for "content")) "Content:") " "
+                   (input (@ (type "text")
+                             (id "content")
+                             (name "content"))))
+                 (div (@ (class "button"))
+                   (button (@ (type "submit")) "Submit")))
+               (div (@ (id "posts"))
+                 ,@(map build-post-sxml posts)))))))))
+
+(define (build-redirect-page)
+  "Generate an HTML page telling the user to return to the main page."
+  (with-output-to-string
+    (lambda ()
+      (begin
+        (display "<!DOCTYPE html>")
+        (newline)
+        (sxml->xml
+          '(html (@ (lang "en"))
+            (body
+              (p "Success!  To return to the list, click "
+                (a (@ (href "#")) "here")
+                "."))))))))
+
+(define (try-to-add-post posts request-body)
+  "Attempts to parse a POST request body to add a new post to the list."
+  (let ((associations
+          (map
+            (lambda (association)
+              (match (string-split association #\=)
+                ((key value)
+                   (cons (parse-url-encoded key)
+                         (parse-url-encoded value)))))
+            (string-split (utf8->string request-body) #\&))))
+    (let ((maybe-title-pair (assoc "title" associations))
+          (maybe-author-pair (assoc "author" associations))
+          (maybe-content-pair (assoc "content" associations)))
+      (if (or (not (pair? maybe-title-pair))
+              (not (pair? maybe-author-pair))
+              (not (pair? maybe-content-pair)))
+        posts
+        (let ((title (cdr maybe-title-pair))
+              (author (cdr maybe-author-pair))
+              (content (cdr maybe-content-pair)))
+          (if (and (> (string-length title) 0)
+                   (<= (string-length title) max-chars-in-title-field)
+                   (> (string-length author) 0)
+                   (<= (string-length author) max-chars-in-author-field)
+                   (> (string-length content) 0)
+                   (<= (string-length content) max-chars-in-content-field))
+            (cons
+              (make-post title author content)
+              (if (= (length posts) limit-of-posts)
+                (list-head posts (- (length posts) 1))
+                posts))
+            posts))))))
+
+(define (handle-request request request-body posts)
+  "Takes all HTTP requests and returns an appropriate HTTP response."
+  (let ((path (uri-path (request-uri request))))
+    (cond
+      ((string=? "/" path)
+        (case (request-method request)
+          ((GET)
+             (values
+               '((content-type . (text/html)))
+               (build-main-page posts)
+               posts))
+          ((POST)
+             (values
+               (build-response
+                 #:code 303
+                 #:headers `((content-type . (text/html))
+                             (location .
+                               ,(build-uri 'http
+                                           #:path (string-append
+                                                     path
+                                                     "#")))))
+               (build-redirect-page)
+               (try-to-add-post posts request-body)))
+          (else
+            (values
+              '((content-type . (text/plain)))
+              (build-main-page posts)
+              posts)))))))
+
+(define (main args)
+  "The entrypoint for the program."
+  (begin
+    (run-server handle-request
+                'http
+                `(#:host ,host
+                  #:port ,port)
+                '())))
+
+(main (command-line))
